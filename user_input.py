0e54581b45ff8636fda6ee7aa7e4312b2148dbcf
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-07-10 11:28:34 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name= input("What is your name? ")
+
+print("Hello "+ name+" !")  # '+' is used for string concatenation
+# if we had uswd ',' it would have printed the string and the variable with a space in between
