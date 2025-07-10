7155864c70aacf7e7174b0afe8ae2d0f4adb7b7f
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-07-10 11:28:58 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+name = "Roy"  # string variable
+age = 30  # integer variable
+height = 5.9  # float variable
+is_student = True   # False   # boolean variable
+
+print("Name:",name, "\nAge:", age, "\nHeight:", height, "\nIs Student:", is_student)
