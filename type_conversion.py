fee0c2c4bf238f01d90125d6e77cd27230a6e691
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-07-19 22:33:12 +0530

    Update type_conversion.py

--- type_conversion.py
+++ type_conversion.py
@@ -6,7 +6,7 @@ age= input("Enter your age ")
 # new_age= age +5  # TypeError: can only concatenate str (not "int") to str2 
 
 # to convert the string to integer we can use int() function
-new_age= int(age)+5
+new_age= int(age)+5 
 print(new_age)
 
 # other such functions are: float(), str(), bool()
