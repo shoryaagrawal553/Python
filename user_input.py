116f46688a728ad0d08c119ae88e1ff5e32a5c9d
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-10-22 11:05:00 +0530

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -2,4 +2,5 @@ name= input("What is your name? ")
 
 print("Hello "+ name+" !")  # '+' is used for string concatenation
 
-# if we had uswd ',' it would have printed the string and the variable with a space in between
+# if we had used ',' it would have printed the string and the variable with a space in between
+
