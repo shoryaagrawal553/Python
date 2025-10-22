c0a4327cf24897832e4740f48336cee773654044
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-10-22 11:05:28 +0530

    Remove duplicate code in user_input.py

--- user_input.py
+++ user_input.py
@@ -3,5 +3,3 @@ name= input("What is your name? ")
 print("Hello "+ name+" !")  # '+' is used for string concatenation
 
 # if we had used ',' it would have printed the string and the variable with a space in between
-
-
