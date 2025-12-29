11e6be6d2ed4516aabb3cc6d6d60897bc718a72c
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-12-29 23:42:09 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
 a=input("Enter a number: ") 
 b=input("Enter another number: ")   
-      
+       
 #sum= a+b   # a=2, b=3 -> output 23  because both a and b are strings hence gets concatinated 
 
 sum= int(a)+ int(b)  # converting a and b to integers before adding them
@@ -8,6 +8,7 @@ print("The sum is: ",sum)
 
 sum2= float(a)+ float(b)
 print("The sum is: ",sum2)    
+
 
 
 
