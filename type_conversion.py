dd0027d43bd99c045d304f19bb9d8e957e05a7b8
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2026-02-10 23:26:25 +0530

    Update type_conversion.py

--- type_conversion.py
+++ type_conversion.py
@@ -1,7 +1,7 @@
 # in python by default all the user input values are of string type
 # so we need to convert them to the required type 
  
-age= input("Enter your age ")
+age= input("Enter your age ") 
 
 # new_age= age +5  # TypeError: can only concatenate str (not "int") to str2 
 
@@ -11,4 +11,5 @@ print(new_age)
 
 # other such functions are: float(), str(), bool()
 print(float(new_age)) # converts to float
+
 
