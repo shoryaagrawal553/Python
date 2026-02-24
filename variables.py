7bbfae00da26555652a39e3d866e40adf2b4309a
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2026-02-24 23:30:51 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,8 @@
 name = "Roy"  # string variable 
 grade = 'A'  # character variable (single character string)
 age = 30  # integer variable  
-height = 5.9  # float variable
+height = 5.9  # float variable 
 is_student = True   # False   # boolean variable
 
 print("Name:",name, "\nAge:", age, "\nHeight:", height, "\nIs Student:", is_student, "\nGrade:", grade)
+
